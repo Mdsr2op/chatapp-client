cd57343d195b9d5ede337afe288ae9e062c27d7c
Mdsr2op <mdsrmfkhan@gmail.com>
2024-08-07 16:39:21 -0700

    added Suspense component for the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,37 @@
-import React, { lazy } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import ProtectedRoute from './components/auth/ProtectedRoute'
+import React, { lazy, Suspense } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProtectedRoute from "./components/auth/ProtectedRoute";
 
-const Home = lazy(() => import("./pages/Home"))
-const Login = lazy(() => import("./pages/Login"))
-const Chat = lazy(() => import("./pages/Chat"))
-const Groups = lazy(() => import("./pages/Groups"))
-const NotFound = lazy(() => import("./components/styles/NotFound"))
+const Home = lazy(() => import("./pages/Home"));
+const Login = lazy(() => import("./pages/Login"));
+const Chat = lazy(() => import("./pages/Chat"));
+const Groups = lazy(() => import("./pages/Groups"));
+const NotFound = lazy(() => import("./components/styles/NotFound"));
 
 const App = () => {
   const user = true;
   return (
     <BrowserRouter>
-      <Routes>
-        <Route element={<ProtectedRoute user={user}/>}>
-          <Route path="/" element={<Home />} />
-          <Route path="/chat/:chatId" element={<Chat />} />
-          <Route path="/groups" element={<Groups />} />
-        </Route>
-        <Route
-         path="/login"
-         element={
-         <ProtectedRoute user={!user} redirect="/">
-          <Login />
-        </ProtectedRoute>} />
-        <Route path="*" element={<NotFound />}></Route>
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route element={<ProtectedRoute user={user} />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/chat/:chatId" element={<Chat />} />
+            <Route path="/groups" element={<Groups />} />
+          </Route>
+          <Route
+            path="/login"
+            element={
+              <ProtectedRoute user={!user} redirect="/">
+                <Login />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
-}
+};
 
-export default App
+export default App;
--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense, useState } from "react";
+import React, { lazy, Suspense, useState } from "react";
 import {
   Box,
   AppBar,
