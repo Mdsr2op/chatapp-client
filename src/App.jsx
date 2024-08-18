f34587bc865bbd38e6e38cd1638040dae30ad617
Mdsr2op <mdsrmfkhan@gmail.com>
2024-08-18 06:41:52 -0700

    Added routes for chat-management, message-management and user-management

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,9 @@ const NotFound = lazy(() => import("./components/styles/NotFound"));
 
 const AdminLogin = lazy(() => import("./pages/admin/AdminLogin"));
 const Dashboard = lazy(() => import("./pages/admin/Dashboard"));
+const MessageManangement = lazy(() => import("./pages/admin/MessageManangement"));
+const ChatManagement = lazy(() => import("./pages/admin/ChatManagement"));
+const UserManagement = lazy(() => import("./pages/admin/UserManagement"));
 
 const App = () => {
   const user = true;
@@ -33,6 +36,9 @@ const App = () => {
           />
           <Route path="/admin" element={<AdminLogin />}/>
           <Route path="/admin/dashboard" element={<Dashboard />}/>
+          <Route path="/admin/chat-management" element={<ChatManagement />}/>
+          <Route path="/admin/message-management" element={<MessageManangement />}/>
+          <Route path="/admin/user-management" element={<UserManagement />}/>
 
           <Route path="*" element={<NotFound />}></Route>
         </Routes>
